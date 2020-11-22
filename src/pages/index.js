3057b8a67b12120fb44babb97fe146c9f751eec4
Pat Chormai <pat.chormai@gmail.com>
2020-11-22 17:56:24 +0100

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,20 +4,29 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import numeral from "numeral"
 
 const data = require("../data.json")
 
-const IndexPage = () => (
-  <Layout>
+const IndexPage = () => {
+  const sortedData = data.map(d => {
+    return {
+      ratio: d["red"] / (d["red"] + d["blue"]),
+      ...d,
+    }
+  })
+
+  sortedData.sort((a, b) => b.ratio - a.ratio)
+
+  return <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Ratio of 🟥/🟦.</p>
+    <h1>It's all about signs!</h1>
+    <p>We're interested in the ratio of 🟥/(🟦 + 🟥).</p>
     <ul>
       {
-        data.map((d, i) => {
+        sortedData.map((d, i) => {
           return <li>
-            <b>{i+1} : {d.filename} | ratio {d["red"]/ d["blue"]}</b>
+            <b>{i+1} : {d.filename} | ratio {numeral(d.ratio).format('1.00')}</b>
             <div>
             <img style={{verticalAlign: "top"}} width="200px" src={`./sign-images/${d.filename}-original.jpg`}/>
             <img style={{verticalAlign: "top"}} width="200px" src={`./sign-images/${d.filename}-mask.jpg`}/>
@@ -26,9 +35,7 @@ const IndexPage = () => (
         })
       }
     </ul>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
-)
+}
 
 export default IndexPage
