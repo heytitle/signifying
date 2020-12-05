e90ed6881065b8be0cb92a98001a3dc8607383c5
Pat Chormai <pat.chormai@gmail.com>
2020-12-05 14:42:58 +0100

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Signifying!`,
+    description: `When the signboards of organizations signify the presence of their executives.`,
     author: `@gatsbyjs`,
   },
   plugins: [
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,9 +9,23 @@ import numeral from "numeral"
 const data = require("../data.json")
 
 const dataDetails = require("../dataDetails.json")
+const includeDummy = false
+
+const Bubble = ({text}) => {
+  return <b style={{
+      borderRadius: `5px`,
+      background: `lightgray`,
+      padding: `0px 5px`,
+      display: `inline-block`,
+      verticalAlign: `middle`,
+      margin: `0px 5px`,
+    }}>
+    {text}
+  </b>
+}
 
 const IndexPage = () => {
-  const sortedData = data.map(d => {
+  const sortedData = data.filter(d => d.filename !== "dummy").map(d => {
     return {
       ratio: d["red"] / (d["red"] + d["blue"]),
       ...d,
@@ -22,21 +36,35 @@ const IndexPage = () => {
 
   return <Layout>
     <SEO title="Home" />
-    <h1>Attention is All You Need!</h1>
-    <p>We're interested in the ratio of 🟥/(🟦 + 🟥).</p>
-    <ul style={{padding: 0}}>
-      {
-        sortedData.map((d, i) => {
-          return <li style={{listStyle: `none`}}>
-            <b>ID={d.filename}</b> | ratio {numeral(d.ratio).format('1.00')} | {dataDetails[d.filename]["orgType"]} | {dataDetails[d.filename]["type"]}
-            <div>
-            <img style={{verticalAlign: "top"}} width="200px" src={`./sign-images/${d.filename}-original.jpg`}/>
-            <img style={{verticalAlign: "top"}} width="200px" src={`./sign-images/${d.filename}-mask.jpg`}/>
-            </div>
-          </li>
-        })
-      }
-    </ul>
+    <div style={{width: `650px`}}>
+      <h1>Signifying!</h1>
+      <p>When the signboards of Thai organizations signify the presence of their executives.</p>
+      <p stle={{background: `lightgray`}}>
+        <b>Remark:</b> Samples below are selected from the internet without any properly predefined selection criteria; hence, <b>statistics derived from them are not respresentative of the population and must be carefully considered when interpreting.</b>
+      </p>
+      <ul style={{padding: 0, marginLeft: `0px`}}>
+        {
+          sortedData.map((d, i) => {
+            return <li style={{listStyle: `none`, textAlign: `left`, marginLeft: `0px`, marginBottom: `20px`}}>
+              <b>
+                Ratio <span style={{fontSize: `12px`, verticalAlign: `center`}}>🟥/(🟦 + 🟥)</span>:
+              </b> {` `}
+              <Bubble text={numeral(d.ratio).format('1.00')}/>
+              {` | `}
+              <a href={dataDetails[d.filename]["src"]}>Source(id={d.filename})</a>
+              <div style={{display: `inline-block`}}>
+                <img style={{verticalAlign: "top", marginBottom: 0}} width="325px" src={`./sign-images/${d.filename}-original.jpg`}/>
+                <img style={{verticalAlign: "top", marginBottom: 0}} width="325px" src={`./sign-images/${d.filename}-mask.jpg`}/>
+                <div style={{marginTop: `5px`, display:`inline-block`}}>
+                  Organization type: <Bubble text={dataDetails[d.filename]["orgType"]}/> 
+                  Format: <Bubble text={dataDetails[d.filename]["type"]}/>
+                </div>
+              </div>
+            </li>
+          })
+        }
+      </ul>
+    </div>
   </Layout>
 }
 
